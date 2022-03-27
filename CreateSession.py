7ff67e8a751fabcba211e1b1d8e3bd679430fa4a
shibbasak <shib.basak@gmail.com>
2022-03-26 23:14:08 -0500

    Create CreateSession.py

---
+++ CreateSession.py
@@ -0,0 +1,17 @@
+import os
+import boto3
+from datetime import datetime, timedelta
+import json
+
+session = boto3.Session(profile_name='default')
+dev_s3_client = session.client('s3',region_name='us-east-1')
+
+glue_job = session.client('glue',region_name='us-east-1')
+
+
+response = glue_job.start_job_run(
+               JobName = 'xxx',
+               Arguments = {
+                 '--some_key':   'Some Value'} )
+
+print(json.dumps(response, indent=4, sort_keys=True, default=str))
